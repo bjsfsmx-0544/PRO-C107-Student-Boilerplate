42c6e51ddac2caad9d032b95299e0057dc1df852
MarioCodi <mario.cortes@byjusfutureschool.com>
2022-02-15 13:07:21 -0600

    Traducción 1.0
    Modificación de comentarios y elementos en pantalla. Actualización de README.md

--- object_tracking.py
+++ object_tracking.py
@@ -4,16 +4,16 @@ import math
 
 video = cv2.VideoCapture("bb3.mp4")
 
-# Load tracker 
+# Cargar rastreador 
 tracker = cv2.TrackerCSRT_create()
 
-# Read the first frame of the video
+# Leer el primer cuadro del video
 returned, img = video.read()
 
-# Select the bounding box on the image
-bbox = cv2.selectROI("Tracking", img, False)
+# Seleccionar el cuadro delimitador en la imagen
+bbox = cv2.selectROI("Rastreando", img, False)
 
-# Initialise the tracker on the img and the bounding box
+# Inicializar el rastreador en la imagen y el cuadro delimitador
 tracker.init(img, bbox)
 
 print(bbox)
@@ -23,36 +23,36 @@ def drawBox(img, bbox):
 
     cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
 
-    cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
+    cv2.putText(img,"Rastreando",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
 
 def goal_track(img, bbox):
     x, y,w, h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
 
-    #################
-    # ADD CODE HERE #
-    #################
+    #######################
+    # AGREGAR CÓDIGO AQUÍ #
+    #######################
 
 while True:
     
     check, img = video.read()   
 
-    # Update the tracker on the img and the bounding box
+    # Actualizar el rastreador en la imagen y el cuadro delimitador
     success, bbox = tracker.update(img)
 
     if success:
         drawBox(img, bbox)
     else:
-        cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+        cv2.putText(img,"Perdido",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
 
-    #################
-    # ADD CODE HERE #
-    #################
+    #######################
+    # AGREGAR CÓDIGO AQUÍ #
+    #######################
 
-    cv2.imshow("result", img)
+    cv2.imshow("Resultado", img)
             
     key = cv2.waitKey(25)
     if key == 32:
-        print("Stopped")
+        print("Detenido")
         break
 
 video.release()
